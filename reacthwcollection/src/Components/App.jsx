40ba9cfc91c8c4c400bf9eb691eb9b3abc31f019
rlmgtr <rlmagtira@gmail.com>
2024-07-16 16:31:59 +0800

    Created template

---
+++ reacthwcollection/src/Components/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'; // Corrected import statement
+import TestComponent from './TestComponent/TestComponent';
+
+const App = () => {
+  return (
+    <div>
+      <h1>This is your empty DOM</h1> 
+      <TestComponent/>
+    </div>
+  );
+}
+
+export default App;
---
+++ reacthwcollection/src/Components/TestComponent/TestComponent.jsx
@@ -0,0 +1,11 @@
+import css from './TestComponent-module.css'
+
+const TestComponent = () => {
+  return (
+    <div className='testComp'>
+      This is a test component only
+    </div>
+  )
+}
+
+export default TestComponent
